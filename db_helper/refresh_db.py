ba00ec99ce5145daf35d1de0031b93c694735a42
Anonymous_5726221 <Anonymous_5726221@protonmail.com>
2021-10-16 22:42:36 +0200

    Tidy up code and add refresh result to output

--- db_helper/refresh_db.py
+++ db_helper/refresh_db.py
@@ -6,20 +6,17 @@ from datetime import datetime as dt
 import psycopg2
 from psycopg2 import sql
 
-from db_helper.tools import _translate_columns_to_db
+from db_helper.tools import _translate_columns_to_db, _translate_columns_from_db, get_all_from_db
 from SeismicPortal.dl_quakes import CANARY_BOX, SeismicPortal
 
-sp = SeismicPortal()
 
-DB_URL = urlparse(os.environ.get("DATABASE"))
+DB_URL = urlparse(os.environ.get("DATABASE_URL"))
 
 DB_HOST = DB_URL.hostname
 DB_PORT = DB_URL.port
 DB_NAME = DB_URL.path[1:]
 DB_USER = DB_URL.username
 DB_PASS = DB_URL.password
-
-conn = psycopg2.connect(f"host={DB_HOST} port={DB_PORT} dbname={DB_NAME} user={DB_USER} password={DB_PASS}")
 
 
 def convert_sp_to_db(q_list):
@@ -31,18 +28,12 @@ def convert_sp_to_db(q_list):
     return to_update
 
 
-results = sp.download_earthquakes(start_time=dt.now() - timedelta(days=5), **CANARY_BOX)
-to_update = convert_sp_to_db([r.get("properties") for r in results])
-
-
-
-def upsert_row(r):
+def upsert_row(r, conn):
 
     col_names = tuple(r.keys())
     vals = ", ".join(f"'{v}'" for v in r.values())
     conflict_resolution = ", ".join(f"{c} = EXCLUDED.{c}" for c in r.keys())
 
-    sql_q = "INSERT INTO quake_data(%s) VALUES(%s) ON CONFLICT (id) DO UPDATE SET %s;"
     sql_q = sql.SQL('INSERT INTO quake_data({col}) VALUES({val}) ON CONFLICT (id) DO UPDATE SET {conf};').format(
         col=sql.SQL(',').join(map(sql.Identifier, col_names)),
         val=sql.SQL(vals),
@@ -57,8 +48,26 @@ def upsert_row(r):
         conn.commit()
 
 
-for ix, r in enumerate(to_update):
-    print(f"Updating {ix+1} of {len(to_update)} rows. ID: {r.get('id')}")
-    upsert_row(r)
+if __name__ == '__main__':
 
+    sp = SeismicPortal()
+    conn = psycopg2.connect(f"host={DB_HOST} port={DB_PORT} dbname={DB_NAME} user={DB_USER} password={DB_PASS}")
 
+    results = sp.download_earthquakes(start_time=dt(2021, 8, 4), **CANARY_BOX)
+    to_update = convert_sp_to_db([r.get("properties") for r in results])
+
+    db_snap = {r.get("unid"): r for r in get_all_from_db(conn)}
+
+    for ix, r in enumerate(to_update):
+        print(f"Updating {ix+1} of {len(to_update)} rows. ID: {r.get('id')}")
+        upsert_row(r, conn)
+
+    db_diff = {r.get("unid"): r for r in get_all_from_db(conn)}
+
+    for k, v in db_diff.items():
+
+        if db_snap.get(k) != v:
+
+            print(f"Event {v.get('unid')} updated:")
+            print(f"Before: {db_snap.get(k)}")
+            print(f"After: {v}")
