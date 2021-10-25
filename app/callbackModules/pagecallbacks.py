93edda5a8304955c07df1e96937b036104b20b34
Anonymous_5726221 <Anonymous_5726221@protonmail.com>
2021-10-25 12:27:38 +0200

    Ugly fix for table updating

--- app/callbackModules/pagecallbacks.py
+++ app/callbackModules/pagecallbacks.py
@@ -26,7 +26,7 @@ def render_page_content(pathname):
     elif pathname == "/treemap":
         content = viewModules.treemapview_layout
     elif pathname == "/statistics":
-        content = viewModules.statsview_layout
+        content = viewModules.statsview_layout()
     else:
         # If the user tries to reach a different page, return a 404 message
         content = dbc.Jumbotron(
