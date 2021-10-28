557eed3446c707d5dbacebe413ae03b3ca211495
Anonymous_5726221 <Anonymous_5726221@protonmail.com>
2021-10-28 18:39:12 +0200

    Move cache declaration in server

--- app/server.py
+++ app/server.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_caching import Cache
+
 from dash import Dash, html
 import dash_bootstrap_components as dbc
 
@@ -14,3 +16,11 @@ external_stylesheets = [
 server = Flask('la-palma-data-viz')
 app = Dash(external_stylesheets=external_stylesheets, server=server)
 app.title = "la-palma-data-viz"
+
+CACHE_CONFIG = {
+    'CACHE_TYPE': 'filesystem',
+    'CACHE_DIR': "/tmp/cached_master_df"
+}
+cache = Cache()
+cache.init_app(app.server, config=CACHE_CONFIG)
+
